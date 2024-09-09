59a7f1fe46f33db61300cefe6c5a1661ced977ee
Hotimpulse <hotimpulse@hotmail.com>
2024-09-09 22:58:02 +0300

    feat: adding a listing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
+import Listing from "./pages/Listing/Listing";
 
 function App() {
-  fetch('http://localhost:8000/orders?_start=0&_limit=10').then((data => {
-    console.log(data)
-  }))
   return (
     <>
-      <div>Hello!</div>
+      <div>
+        <Listing />
+      </div>
     </>
   );
 }
---
+++ src/pages/Listing/Listing.tsx
@@ -0,0 +1,21 @@
+export default function Listing() {
+  return (
+    <div>
+      <div className="card">
+        <div className="top-part">
+          <div className="heading">
+            <h1>heading</h1>
+          </div>
+          <div className="price">X rub</div>
+        </div>
+        <div className="lower-part">
+          <img src="./" alt="image of the product" />
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+            cum culpa neque molestiae...
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
