c665518a0a261c08e454c53529972eeba853fbd1
Hotimpulse <hotimpulse@hotmail.com>
2024-09-10 23:28:40 +0300

    fix: fixing some styling for the all listings page

--- src/pages/AllListings/AllListings.tsx
+++ src/pages/AllListings/AllListings.tsx
@@ -25,20 +25,28 @@ export default function AllListings() {
   }, []);
 
   return (
-    <div className={listingStyles.listings_container}>
-      {listings.map((listing) => (
-        <div className={listingStyles.listings} key={listing.id}>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={listing.imageUrl} />
-            <Card.Body>
-              <Card.Title>{listing.name}</Card.Title>
-              <h4>Цена: {listing.price}</h4>
-              <h4>Описание: </h4>
-              <Card.Text>{listing.description}</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
+    <div className={listingStyles.listings_wrapper}>
+      <div className={listingStyles.listing_container}>
+        {listings.map((listing) => (
+          <div className={listingStyles.listings} key={listing.id}>
+            <Card className={listingStyles.listing}>
+              <Card.Img
+                className={listingStyles.listing_img}
+                variant="top"
+                src={listing.imageUrl}
+              />
+              <Card.Body className={listingStyles.listing_body}>
+                <Card.Title>{listing.name}</Card.Title>
+                <h4>Цена: {listing.price}</h4>
+                <h4>Описание: </h4>
+                <Card.Text className={listingStyles.listing_desc}>
+                  {listing.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
       <div>pagination</div>
     </div>
   );
