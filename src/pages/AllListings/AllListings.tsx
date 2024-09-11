0643f2b383048a4e54aaed8ffe39350c0fed5141
Hotimpulse <hotimpulse@hotmail.com>
2024-09-11 12:39:59 +0300

    feat: adding a redux store and an ads slice

--- src/pages/AllListings/AllListings.tsx
+++ src/pages/AllListings/AllListings.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import listingStyles from "./allListings.module.scss";
 import { IAdvertisment } from "@src/interfaces/IAdvertisment";
+import { Button } from "react-bootstrap";
 
 export default function AllListings() {
   const [listings, setListings] = useState<IAdvertisment[]>([]);
@@ -43,9 +44,10 @@ export default function AllListings() {
                 <h4>Цена: {listing.price}</h4>
                 <h4>Описание: </h4>
                 <Card.Text className={listingStyles.listing_desc}>
-                  {listing.description}
+                  {listing.description !== "" ? listing.description : "No desc"}
                 </Card.Text>
               </Card.Body>
+              <Button>Open</Button>
             </Card>
           </div>
         ))}
--- src/store/AllListings/allListingsSlice.ts
+++ src/store/AllListings/allListingsSlice.ts
@@ -1,10 +1,42 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IAdvertisment } from "@src/interfaces/IAdvertisment";
 
 interface IAdvertisments {
   listings: IAdvertisment[];
-  status: string;
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+  currentPage: number;
+  totalPages: number;
 }
+
 const initialState: IAdvertisments = {
   listings: [],
-  status: "", // 'loading', 'error', 'ready'
+  status: "idle",
+  error: null,
+  currentPage: 1,
+  totalPages: 1,
 };
+
+const adSlice = createSlice({
+  name: "ads",
+  initialState,
+  reducers: {
+    setAds(state, action: PayloadAction<IAdvertisment[]>) {
+      state.listings = action.payload;
+    },
+    setCurrentPage(state, action: PayloadAction<number>) {
+      state.currentPage = action.payload;
+    },
+    setTotalPages(state, action: PayloadAction<number>) {
+      state.totalPages = action.payload;
+    },
+    setError(state, action: PayloadAction<string>) {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setAds, setCurrentPage, setTotalPages, setError } =
+  adSlice.actions;
+
+export default adSlice.reducer;
